7477a8d33d653c6f51954a46e5205a6f41cdd74c
Aryan Kaushik <aryan.kaushik2015@gmail.com>
2025-06-10 00:11:26 +0530

    V2 Deployment

--- shared/auth_service.py
+++ shared/auth_service.py
@@ -1,26 +1,11 @@
 from functools import wraps
-from dotenv import load_dotenv
 from flask import request, jsonify, g
 import firebase_admin
 from firebase_admin import credentials, auth
-import os
 
-load_dotenv()
-# Initialize Firebase with the dict
-firebase_cred_dict = {
-    "type": os.getenv("FIREBASE_TYPE"),
-    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
-    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
-    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
-}
-cred = credentials.Certificate(firebase_cred_dict)
+FIREBASE_CRED_PATH = "/etc/secrets/firebase-sdk.json"
+
+cred = credentials.Certificate(FIREBASE_CRED_PATH)
 firebase_admin.initialize_app(cred)
 
 def verify_token(f):
